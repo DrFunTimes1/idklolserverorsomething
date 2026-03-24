1af287ea66050d40954b83b575c2df98233362ba
Vihaan Vashisth <vihaanvashisth2207@gmail.com>
2026-03-24 07:43:45 +0000

    make the stopall.sh script also commit to the server before shutting down

--- scripts/stopall.sh
+++ scripts/stopall.sh
@@ -10,6 +10,8 @@ sleep 5
 screen -S mc -X quit
 screen -S playit -X quit
 screen -S idle -X quit
-sleep 10
+
+git add . ; git commit -m "something changed in the world" ; git push origin main --force
+sleep 20
 
 gh codespace stop -c silver-space-barnacle-jjwp75v9xx77h5j9jsilver-space-barnacle-jjwp75v9xx77h5j9j
